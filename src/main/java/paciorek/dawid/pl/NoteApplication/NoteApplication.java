a7e8c39d923fae2569f5378edb5b50286440991c
Dawid24 <paciorekdawidonly@gmail.com>
2018-10-27 10:41:23 +0200

    first commit

---
+++ src/main/java/paciorek/dawid/pl/NoteApplication/NoteApplication.java
@@ -0,0 +1,12 @@
+package paciorek.dawid.pl.NoteApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteApplication.class, args);
+	}
+}

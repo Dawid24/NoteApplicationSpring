ff09a310c63475fb36276167d21a435bf9def8b7
Dawid24 <paciorekdawidonly@gmail.com>
2018-10-27 12:02:47 +0200

    add NoteRepository

---
+++ src/main/java/paciorek/dawid/pl/NoteApplication/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package paciorek.dawid.pl.NoteApplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import paciorek.dawid.pl.NoteApplication.model.Note;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    
+}
